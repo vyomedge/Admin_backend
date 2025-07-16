682d28ca42b5470eac115110af38135fc4041c46
swapnil9340 <swapnilchauhan08@gmail.com>
2025-07-16 19:03:19 +0530

    add cors error in spp .js

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const express = require('express');
+                                            const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, origin || '*');
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/blogs', require('./routes/blogRoutes'));
 app.use('/api/portfolio', require('./routes/portfolioRoutes'));
