e7a8ee521f3dd8fa4caadabe563bb86c8317857d
swapnil9340 <swapnilchauhan08@gmail.com>
2025-07-18 18:17:07 +0530

    chnage 8089

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
